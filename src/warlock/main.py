4e80ebea9d6cc459ed933ef4afe10f52306126af
Roger Worrell <raworre@gmail.com>
2026-06-29 15:33:34 -0600

    WBW-21 disable dns rebinding protection

--- src/warlock/main.py
+++ src/warlock/main.py
@@ -43,6 +43,7 @@ def main():
         logger.info(f"Starting Warlock MCP Server in SSE Mode on http://{args.host}:{args.port}")
         mcp.settings.host = args.host
         mcp.settings.port = int(args.port)
+        mcp.settings.transport_security.enable_dns_rebinding_protection = False
         mcp.run(transport="sse")
     else:
         logger.info("Starting Warlock MCP Server in stdio mode")
